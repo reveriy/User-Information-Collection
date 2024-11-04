def6a84c7bb917e43891c6637fbd40caf338fe37
Valorie Portem <valorieportem@gmail.com>
2024-11-04 16:59:56 +0800

    git commit -m "initial commit"

---
+++ userProfile.js
@@ -0,0 +1,10 @@
+let firstName = "Valorie";
+let lastName = "Portem";
+let Age = "19 y/o";
+let favNum = "21";
+let favColor = "Blue";
+
+console.log(firstName + " " + lastName);
+console.log(Age);
+console.log(favNum);
+console.log(favColor);
